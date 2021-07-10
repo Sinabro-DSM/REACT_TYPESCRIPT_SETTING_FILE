d76fdca1e67c2851617b5f122c91d35472987df4
jiwon <jidole041214@naver.com>
2021-07-10 20:48:14 +0900

    [CREATE] feed

---
+++ src/components/feed/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import FeedCardList from '../public/feedCardList'
+import * as S from './styles'
+
+export default function Feed(){
+    return(
+        <>
+        <FeedCardList/>
+        </>
+    )
+}
