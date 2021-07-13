f4b50cf34ec9e71d9f2cc80dda15530433f1af0c
seockhun <hun00719@gmail.com>
2021-07-13 16:36:28 +0900

    [CREATE] MyPage UI

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,5 @@
 export { default as Heart } from "./heart";
 export { default as Logo } from "./logo.png";
 export { default as Search } from "./search";
-export { default as LogoWithout } from "./logowithout.png"
+export { default as LogoWithout } from "./logowithout.png";
+export { default as ProfileImg } from "./profileImg.png";
